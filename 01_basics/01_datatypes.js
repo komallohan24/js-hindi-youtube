afd2aac1cee2a9df9751943f1fa23a5e748e51d5
komallohan24 <komallohan24@gmail.com>
2025-04-25 17:18:41 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node js , not browser
+
+console.log(3+
+    3) //code readability should be high
+
+console.log("Komal")
+
+
+
+let name = "komal"
+let age = 18
+let isLoggedIn = true
+let state;
+
+
+
+// number => 2 to power 53
+// bigint
+// string  => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof "komal");
+console.log(typeof null); //object
+console.log(typeof age);
+console.log(typeof undefined); //undefined
