fc8d950c5d175a8c339997e2d488b149e09d9495
komallohan24 <komallohan24@gmail.com>
2025-04-25 18:42:40 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "komal"              
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan (type is number) not a number full form
+//  "true" => 1; false => 0
+
+
+let isLoggedIn = "komal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "komal" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
