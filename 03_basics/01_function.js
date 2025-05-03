1d13810b3844ca816529f602b9f615cd2c6e41ce
komallohan24 <komallohan24@gmail.com>
2025-05-03 17:03:27 +0000

    function done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -39,4 +39,35 @@ console.log("Please enter a username");
     return `${username} just logged in `
 }
 
-console.log(loginUserMessage("komal"));
+// console.log(loginUserMessage("komal"));
+
+function calculateCartPrice(val1 , val2, ...num1){
+return num1
+}
+
+// console.log(calculateCartPrice(200, 400 , 500, 2000))
+
+const user = {
+    username : "komal",
+    price : 299
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    }
+
+    // handleObject(user)
+    handleObject({
+        username : "Sam" , 
+        price : 399
+    })
+
+    const myNewArray = [200, 400, 100, 600]
+
+    function returnSecondValue(getArray){
+        return getArray[1]
+    }
+
+    // console.log(returnSecondValue(myNewArray));
+    console.log(returnSecondValue([200, 400, 500, 1000]));
+    
