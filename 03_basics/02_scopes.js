faef98f7d2ee7a12b270bfa31b9cd31ac715d7c8
komallohan24 <komallohan24@gmail.com>
2025-05-05 17:11:38 +0000

    nested done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,15 +6,58 @@ if(true){
     let a = 10
  const b =20
 //  var c = 30
-console.log("INNER :" , a);
+// console.log("INNER :" , a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array [i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array [i];
     
-}
+// }
 
-console.log(a); 
+// console.log(a); 
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "komal"
+
+    function  two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "komal"
+    if(username === "komal"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// ++++++++++++++++ interesting ++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+console.log(addTwo(5));
+
+const addTwo = function(num){  // ya bhi function but esko kafi time expression bhi bol dia jata ha
+    return num + 2
+}
 
