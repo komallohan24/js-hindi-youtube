8450cc26a6d969e52e0bc33b50388de3791dfa72
komallohan24 <komallohan24@gmail.com>
2025-05-07 19:08:06 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+//  if statement
+const isUserloggedIn = true
+
+// if(2 != 3){
+//  console.log("exceuted");
+ 
+// }
+// const temperature = 41
+// if (temperature === 40){
+//     console.log("less than 50");
+    
+// } else {
+//     console.log("temperature is greater than 50");
+    
+// }
+// console.log("Execute");
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+// < , > , <=, >= , == , != ,=== , !==
+
+const balance = 1000
