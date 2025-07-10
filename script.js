c8f62485328e476d0daaee4ceb780967eb5f3a9c
komallohan24 <komallohan24@gmail.com>
2025-07-10 12:31:40 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,29 @@
+const menuIcon = document.getElementById("menuIcon");
+const navLinks = document.getElementById("navLinks");
+
+menuIcon.addEventListener("click" , () => {
+    navLinks.classList.toggle("open");
+})
+
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".nav-links li a");
+
+window.addEventListener("scroll",()=>{
+    let current="";
+
+    sections.forEach((section)=>{
+        const sectionTop=section.offsetTop-60;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight){
+            current = section.getAttribute("id");
+        }
+    });
+
+    navItems.forEach((link)=>{
+        link.classList.remove("active")
+        if (link.getAttribute("href")===`#${current}`){
+            link.classList.add("active");
+        }
+    });
+});
